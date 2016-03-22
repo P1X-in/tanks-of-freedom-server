07c3df4527de42d2e522e5aaf0f2db3d9522eaf4
CzlowiekImadlo <quak@tlen.pl>
2016-03-22 21:42:24 +0100

    Add a bit of comments

--- tof_server/__init__.py
+++ tof_server/__init__.py
@@ -1,4 +1,7 @@
+"""Module init."""
 from flask import Flask
 
+
 app = Flask(__name__)
+
 from tof_server import views
--- tof_server/views.py
+++ tof_server/views.py
@@ -1,6 +1,9 @@
+"""This module provides views for application."""
 from tof_server import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
+    """Index test action"""
     return "Hello, World!"
