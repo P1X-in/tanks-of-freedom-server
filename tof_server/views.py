2686681965c2deef4f9bd10891f62d893c4acec0
CzlowiekImadlo <quak@tlen.pl>
2016-03-22 22:00:46 +0100

    Add stub id generator

--- tof_server/views.py
+++ tof_server/views.py
@@ -1,9 +1,14 @@
 """This module provides views for application."""
 from tof_server import app
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     """Index test action"""
     return "Hello, World!"
+
+@app.route('/players')
+def generate_new_id():
+    """Method for generating new unique player ids"""
+    return jsonify({ 'id' : 'somestubid' })
