e430ddae5caa1b2d4e9d3161ee6a1b5447d60625
CzlowiekImadlo <quak@tlen.pl>
2016-03-24 22:46:46 +0100

    Generate random pin

--- tof_server/views.py
+++ tof_server/views.py
@@ -1,6 +1,7 @@
 """This module provides views for application."""
 from tof_server import app, versioning, mysql
 from flask import jsonify, make_response
+import string, random
 
 @app.route('/')
 def index():
@@ -15,10 +16,15 @@ def generate_new_id():
     """Method for generating new unique player ids"""
     try:
         cursor = mysql.connection.cursor()
+        new_pin = ''
+
+        characters_pool = string.ascii_uppercase + string.digits
+        for _ in range(8):
+            new_pin = new_pin + random.SystemRandom().choice(characters_pool)
 
         return jsonify({
             'id' : 'somestubid',
-            'pin' : 'stubpin'
+            'pin' : new_pin
         })
     except Exception as er_msg:
         return make_response(jsonify({
