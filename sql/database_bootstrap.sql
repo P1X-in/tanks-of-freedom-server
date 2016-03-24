8ea601fda5aead7cccb009cecf44a33123500cc9
CzlowiekImadlo <quak@tlen.pl>
2016-03-24 20:52:31 +0100

    Added database sql template

---
+++ sql/database_bootstrap.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE players (
+    id INT NOT NULL auto_increment,
+    creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    auto_pin VARCHAR(8) NOT NULL
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE maps (
+    id INT NOT NULL auto_increment,
+    map_hash VARCHAR(32) NOT NULL,
+    player_id INT NOT NULL,
+    creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE maps_data (
+    map_id INT NOT NULL,
+    json TEXT NOT NULL,
+    PRIMARY KEY (map_id)
+);
