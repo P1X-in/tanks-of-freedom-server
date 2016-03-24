3044b76a8bf9894b51e61f1761dfadf1764c9a10
CzlowiekImadlo <quak@tlen.pl>
2016-03-24 20:26:44 +0100

    Add mysql connection

--- tof_server/__init__.py
+++ tof_server/__init__.py
@@ -1,7 +1,11 @@
 """Module init."""
 from flask import Flask
+from flask.ext.mysqldb import MySQL
 
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config.from_object('config')
+mysql.init_app(app)
 
 from tof_server import views
