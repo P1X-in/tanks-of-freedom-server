b9af71959bca5ccd6536994d8f56715bc70b0a57
CzlowiekImadlo <quak@tlen.pl>
2016-03-24 21:33:44 +0100

    Fix loading config

--- tof_server/__init__.py
+++ tof_server/__init__.py
@@ -5,7 +5,7 @@ from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
 mysql = MySQL()
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 mysql.init_app(app)
 
 from tof_server import views
