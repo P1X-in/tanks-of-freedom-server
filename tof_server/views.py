3e2746de9aae541880fe4cf643520a2577a3a0d5
CzlowiekImadlo <quak@tlen.pl>
2016-03-29 22:14:12 +0200

    Add stub methods for map handling

--- tof_server/views.py
+++ tof_server/views.py
@@ -14,30 +14,40 @@ def index():
 @app.route('/players', methods=['POST'])
 def generate_new_id():
     """Method for generating new unique player ids"""
-    try:
-        cursor = mysql.connection.cursor()
-        new_pin = ''
+    cursor = mysql.connection.cursor()
+    new_pin = ''
 
-        characters_pool = string.ascii_uppercase + string.digits
-        for _ in range(8):
-            new_pin = new_pin + random.SystemRandom().choice(characters_pool)
+    characters_pool = string.ascii_uppercase + string.digits
+    for _ in range(8):
+        new_pin = new_pin + random.SystemRandom().choice(characters_pool)
 
-        insert_sql = "INSERT INTO players (auto_pin) VALUES (%s)"
-        id_sql = "SELECT LAST_INSERT_ID()"
+    insert_sql = "INSERT INTO players (auto_pin) VALUES (%s)"
+    id_sql = "SELECT LAST_INSERT_ID()"
 
-        cursor.execute(insert_sql, (new_pin,))
-        cursor.execute(id_sql)
+    cursor.execute(insert_sql, (new_pin,))
+    cursor.execute(id_sql)
 
-        insert_data = cursor.fetchone()
+    insert_data = cursor.fetchone()
 
-        mysql.connection.commit()
-        cursor.close()
+    mysql.connection.commit()
+    cursor.close()
 
-        return jsonify({
-            'id' : insert_data[0],
-            'pin' : new_pin
-        })
-    except Exception as er_msg:
-        return make_response(jsonify({
-            'error' : str(er_msg)
-        }), 500)
+    return jsonify({
+        'id' : insert_data[0],
+        'pin' : new_pin
+    })
+
+@app.route('/maps', methods=['POST'])
+def upload_new_map():
+    """Method for uploading new map"""
+    return jsonify({
+        'code' : 'dummy'
+    })
+
+@app.route('/maps/<string:map_code>', methods=['GET'])
+def download_map(map_code):
+    """Method for downloading a map"""
+    return jsonify({
+        'code' : map_code,
+        'data' : 'dummy'
+    })
