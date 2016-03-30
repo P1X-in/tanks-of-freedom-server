f9f3074a176abdc9586ce897c368afedbdb55a55
CzlowiekImadlo <quak@tlen.pl>
2016-03-30 21:26:23 +0200

    Just use stock json instead of flask

--- tof_server/map_validator.py
+++ tof_server/map_validator.py
@@ -1,15 +1,12 @@
 """Module for validating if map data is correct"""
-from tof_server import app
-from flask import jsonify
-import hashlib
+import hashlib, json
 
 def validate(map_data, cursor):
     """Method for validating if map data is correct"""
 
-    with app.app_context():
-        md5_hash = hashlib.md5(jsonify(map_data).encode())
+    md5_hash = hashlib.md5(json.dumps(map_data).encode())
 
-        return {
-            'status' : 'ok',
-            'result' : md5_hash.hexdigest()
-        }
+    return {
+        'status' : 'ok',
+        'result' : md5_hash.hexdigest()
+    }
