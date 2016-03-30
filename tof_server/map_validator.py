cd7a01f391f2570affd37cf1aa90f215604cea1f
CzlowiekImadlo <quak@tlen.pl>
2016-03-30 21:15:44 +0200

    Add app context

--- tof_server/map_validator.py
+++ tof_server/map_validator.py
@@ -1,13 +1,15 @@
 """Module for validating if map data is correct"""
+from tof_server import app
 from flask import jsonify
 import hashlib
 
 def validate(map_data, cursor):
     """Method for validating if map data is correct"""
 
-    md5_hash = hashlib.md5(jsonify(map_data).encode())
+    with app.app_context():
+        md5_hash = hashlib.md5(jsonify(map_data).encode())
 
-    return {
-        'status' : 'ok',
-        'result' : md5_hash.hexdigest()
-    }
+        return {
+            'status' : 'ok',
+            'result' : md5_hash.hexdigest()
+        }
