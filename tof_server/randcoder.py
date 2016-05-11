97221f28d6da830d96a8fa94bec380f3f78e0867
CzlowiekImadlo <quak@tlen.pl>
2016-05-11 18:07:19 +0200

    Changed random character pool to exclude confusing characters

--- tof_server/randcoder.py
+++ tof_server/randcoder.py
@@ -1,11 +1,11 @@
 """Module for random codes"""
-import string, random
+import random
 
 def get_random_code(length):
     """Method for generating random codes of set length"""
     new_code = ''
 
-    characters_pool = string.ascii_uppercase + string.digits
+    characters_pool = "ACDEFHJKLMNPRTUVWXY3479"
     for _ in range(length):
         new_code = new_code + random.SystemRandom().choice(characters_pool)
 
