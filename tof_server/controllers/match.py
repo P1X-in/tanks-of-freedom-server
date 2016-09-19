046c9c9d3e386e607dafd7ebdc5301ae890bacc3
czlowiekimadlo <quak@tlen.pl>
2016-09-19 17:06:12 +0200

    Added more stub methods to matches controller

--- tof_server/controllers/match.py
+++ tof_server/controllers/match.py
@@ -4,8 +4,65 @@ from tof_server.validators import auth
 
 controller_match = Blueprint('controller_match', __name__, template_folder='templates')
 
-@controller_match.route('/matches', methods=['GET'])
+
+@controller_match.route('/matches/my', methods=['POST'])
 def get_player_matches():
+    """Method for downloading player active matches."""
+    validation = auth.validate(request)
+    if validation['status'] != 'ok':
+        abort(validation['code'])
+
+    return jsonify({
+        'test': 'ok'
+    })
+
+
+@controller_match.route('/matches', methods=['POST'])
+def create_new_match():
+    """Method for downloading player active matches."""
+    validation = auth.validate(request)
+    if validation['status'] != 'ok':
+        abort(validation['code'])
+
+    return jsonify({
+        'test': 'ok'
+    })
+
+
+@controller_match.route('/match/<string:match_code>.json', methods=['GET'])
+def get_match_details(match_code):
+    """Method for downloading player active matches."""
+    return jsonify({
+        'test': 'ok'
+    })
+
+
+@controller_match.route('/match/<string:match_code>.json', methods=['POST'])
+def get_match_state(match_code):
+    """Method for downloading player active matches."""
+    validation = auth.validate(request)
+    if validation['status'] != 'ok':
+        abort(validation['code'])
+
+    return jsonify({
+        'test': 'ok'
+    })
+
+
+@controller_match.route('/match/join/<string:match_code>.json', methods=['POST'])
+def join_match(match_code):
+    """Method for downloading player active matches."""
+    validation = auth.validate(request)
+    if validation['status'] != 'ok':
+        abort(validation['code'])
+
+    return jsonify({
+        'test': 'ok'
+    })
+
+
+@controller_match.route('/match/turn/<string:match_code>.json', methods=['POST'])
+def update_match_state(match_code):
     """Method for downloading player active matches."""
     validation = auth.validate(request)
     if validation['status'] != 'ok':
