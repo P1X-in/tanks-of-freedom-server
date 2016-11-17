e00ddcf4fa9229c9a5af3e74f061e625af73e09e
czlowiekimadlo <quak@tlen.pl>
2016-11-17 18:13:33 +0100

    Added browser integration

--- tof_server/controllers/main.py
+++ tof_server/controllers/main.py
@@ -1,5 +1,6 @@
 """Main controller blueprint."""
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, redirect
+from tof_server import config
 from tof_server.validators import versioning
 
 controller_main = Blueprint('controller_main', __name__, template_folder='templates')
@@ -8,8 +9,11 @@ controller_main = Blueprint('controller_main', __name__, template_folder='templa
 @controller_main.route('/')
 def index():
     """Server information."""
-    return jsonify({
-        'server-version': versioning.SERVER_VERSION,
-        'client-versions': versioning.CLIENT_VERSIONS,
-        'you': request.user_agent.string
-    })
+    if config.INTEGRATE_MAP_BROWSER:
+        return redirect("/browser", code=302)
+    else:
+        return jsonify({
+            'server-version': versioning.SERVER_VERSION,
+            'client-versions': versioning.CLIENT_VERSIONS,
+            'you': request.user_agent.string
+        })
