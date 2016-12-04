eb66f4326d70cd84e5a60866ca5f8720fe68d718
CzlowiekImadlo <quak@tlen.pl>
2016-12-04 18:10:23 +0100

    Fixed broken migration

--- sql/migration_003.sql
+++ sql/migration_003.sql
@@ -5,7 +5,7 @@ INSERT INTO versioning (version_number) VALUES (3);
 CREATE TABLE maps_downloads (
     map_id INT NOT NULL,
     creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (map_id)
+    PRIMARY KEY (map_id, creation_time)
 );
 
 COMMIT;
