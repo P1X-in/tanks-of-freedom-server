99cb36ee18e93c4a05369ecf2a65f018c31460ee
CzlowiekImadlo <quak@tlen.pl>
2017-02-06 17:01:28 +0100

    Added client 0.6.3

--- tof_server/validators/versioning.py
+++ tof_server/validators/versioning.py
@@ -1,7 +1,7 @@
 """Module for handling server and client versions."""
 
 SERVER_VERSION = '0.3.0'
-CLIENT_VERSIONS = ['0.5.2', '0.5.3', '0.6.0', '0.6.1', '0.6.2']
+CLIENT_VERSIONS = ['0.5.2', '0.5.3', '0.6.0', '0.6.1', '0.6.2', '0.6.3']
 
 
 def validate(request):
