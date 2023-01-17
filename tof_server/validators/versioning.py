79348b2961e2e9944c33f73bf9ec00e9f271dd34
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-01-17 15:11:29 +0100

    Bump server version and accept ToF-II

--- tof_server/validators/versioning.py
+++ tof_server/validators/versioning.py
@@ -1,13 +1,19 @@
 """Module for handling server and client versions."""
 
-SERVER_VERSION = '0.4.0'
+SERVER_VERSION = '0.5.0'
 CLIENT_VERSIONS = ['0.5.2', '0.5.3', '0.6.0', '0.6.1', '0.6.2', '0.6.3', '0.7.0']
+CLIENT2_VERSIONS = ['0.3.0']
 
 
 def validate(request):
     """Method for validating client version."""
     for acceptable_version in CLIENT_VERSIONS:
         if request.user_agent.string == 'ToF/' + acceptable_version:
+            return {
+                'status': 'ok'
+            }
+    for acceptable_version in CLIENT2_VERSIONS:
+        if request.user_agent.string == 'ToF-II/' + acceptable_version:
             return {
                 'status': 'ok'
             }
