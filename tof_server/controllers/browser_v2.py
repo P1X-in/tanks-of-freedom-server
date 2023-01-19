a56d4a0220758da1abad36b06da50fb0ff126bc5
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2023-01-19 14:18:22 +0100

    Fix typo

--- tof_server/controllers/browser_v2.py
+++ tof_server/controllers/browser_v2.py
@@ -2,10 +2,10 @@
 from flask import Blueprint, jsonify
 from tof_server.models import map_v2 as map_model
 
-controller_browser = Blueprint('controller_browser_v2', __name__, template_folder='templates')
+controller_browser_v2 = Blueprint('controller_browser_v2', __name__, template_folder='templates')
 
 
-@controller_browser.route('/v2/listing', methods=['GET'])
+@controller_browser_v2.route('/v2/listing', methods=['GET'])
 def first_page():
     """Page for browser maps listing."""
     maps_listing_page = map_model.find_maps_page()
@@ -15,7 +15,7 @@ def first_page():
     })
 
 
-@controller_browser.route('/v2/listing/<int:offset_id>', methods=['GET'])
+@controller_browser_v2.route('/v2/listing/<int:offset_id>', methods=['GET'])
 def offset_page(offset_id):
     """Page for browser maps listing."""
     maps_listing_page = map_model.find_maps_page(offset_id)
@@ -25,7 +25,7 @@ def offset_page(offset_id):
     })
 
 
-@controller_browser.route('/v2/author/<string:map_code>', methods=['GET'])
+@controller_browser_v2.route('/v2/author/<string:map_code>', methods=['GET'])
 def player_page(map_code):
     """Page for listing other maps made by author of a map."""
     maps_listing_page = map_model.find_maps_by_map_author(map_code)
@@ -35,7 +35,7 @@ def player_page(map_code):
     })
 
 
-@controller_browser.route('/v2/top/downloads', methods=['GET'])
+@controller_browser_v2.route('/v2/top/downloads', methods=['GET'])
 def top_downloads_page():
     """Page for listing most downloaded maps."""
     maps_listing_page = map_model.find_maps_top_downloads()
