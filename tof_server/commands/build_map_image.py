a37a95627bfce09a4823da028f7d5df44c830c10
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2024-02-29 12:22:13 +0100

    Command tests

--- tof_server/commands/build_map_image.py
+++ tof_server/commands/build_map_image.py
@@ -2,13 +2,18 @@
 import click
 from flask import Blueprint
 from tof_server.models import map_v2 as map_model
+from tof_server.utils import png_creator_v2 as png_creator
+from tof_server.utils import file_storage
 
 build_map_image_command = Blueprint('build-map-image', __name__, cli_group='admin')
 
 
 @build_map_image_command.cli.command('build-map-image')
-@click.argument('name')
-def execute(name):
+@click.argument('code')
+def execute(code):
     """Generate missing images."""
-    print(name)
+    map_data = file_storage.get_map_v2(code)
+    print(map_data["metadata"])
+    # png_creator.create_map(code, map_data)
+
     # map_model.generate_missing_images()
