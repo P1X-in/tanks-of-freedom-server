2bdcdfe841b55a6423a332ff6a1dad6fb0cb26f3
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2024-02-29 12:10:04 +0100

    Command tests

--- tof_server/commands/__init__.py
+++ tof_server/commands/__init__.py
@@ -1,8 +1,7 @@
 """Load commands blueprints."""
 from tof_server import app
 from tof_server.commands.build_map_images import build_map_images_command
-from tof_server.command.build_map_image import build_map_image_command
+from tof_server.commands.build_map_image import build_map_image_command
 
 app.register_blueprint(build_map_images_command)
 app.register_blueprint(build_map_image_command)
-
